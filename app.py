c9b68e527d545f1e41db74f69f1b46f368ac84cd
ramandrosoa <minah.ram2000@gmail.com>
2026-03-15 01:48:15 +0300

    edit app

--- app.py
+++ app.py
@@ -636,4 +636,4 @@ def dev_set_stage(stage):
 
 # ─── RUN ────────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
